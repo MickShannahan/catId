886459b98a2c961c497eae59ecdd4bd1d4cacfca
mick shannahan <beasticorn@gmail.com>
2024-04-27 09:02:37 -0600

    sometimes returned an empty string

--- catId.js
+++ catId.js
@@ -1,6 +1,6 @@
 const sur = ['Mr. ', 'Lady ', 'Sir ', 'Big ', 'Professor ', 'Capitan ', 'Mayor ', 'Mc ', 'Master ', 'Prime Meowster ', 'Gran-', 'Madam ', 'Her Royal Highness ', 'Duchess ', 'Baroness ', 'Countess ', 'Mistress ', 'Vice Chancellor ', 'Elder ', 'The Venerable ', '']
 const start = ['Fizz', 'Wiggle', 'Fuzz', 'Mouse', 'Frisk', 'Scruf', 'Hairy', 'Klaus', 'Mocha', 'Mert', 'Poppy', 'Butter', 'Caramel', '', 'Potato', 'Nip', 'Bean', 'Vanilla', 'Snibbly', 'Filtch', 'Norris', 'Grumpy', 'Whisker', 'Simba', 'Teddy', 'Abraham', 'Taco', 'Bert', 'Bern', 'Tiger', 'Dima', 'Aleksandr',]
-const end = ['s', 'bum', '-buttons', 'ington', ' esquire', 'mellow', 'wumps', 'worth', ' Blackwell', ' O,neal', ' .jr', 'butt', 'smith', 'bottom', 'paws', 'loaf', 'ordinal', 'buzz', '-shakur', ' The Fierce', 'kitty',' Hobbes', '', '', ]
+const end = ['s', 'bum', '-buttons', 'ington', ' esquire', 'mellow', 'wumps', 'worth', ' Blackwell', ' O,neal', ' .jr', 'butt', 'smith', 'bottom', 'paws', 'loaf', 'ordinal', 'buzz', '-shakur', ' The Fierce', 'kitty', ' Hobbes', '', '',]
 const cats = ['/ᐠ｡ꞈ｡ᐟ\\ﾉ', '/ᐠ.ꞈ.ᐟ\\ﾉ', '/ᐠ_ ꞈ _ᐟ\\ﾉ', '/ᐠ .⋏. ᐟ\\ﾉ', '/ᐠﹷ ‸ ﹷ ᐟ\\ﾉ', '/ᐠ . ֑ . ᐟ\\ﾉ',
   '/ᐠܻܻ   ့⑅ܻᐟ\\', '/ᐠ𝅒_𝅒ᐟ\\ﾉ', '/ᐠ ̷  ̷𝅒 ̷‸ ̷𝅒 ̷ ᐟ\\ﾉ', '/ᐠ-ᆽ-ᐟ \\ﾉ', '/ᐠ ̥  ̮  ̥ ᐟ\\ฅ', '/ᐠ ̥    ̣̮ ̥ ᐟ\\ﾉ', '/ᐠ ̞  ‸  ̞ᐟ\\ﾉ', '(ﾐዋ ﻌ ዋﾐ)ﾉ', '(๑ↀᆺↀ๑)', '(ﾐΦ ﻌ Φﾐ)ﾉ', '(ﾐዕᆽዕﾐ)', '(˵Φ ω Φ˵)', '(ﾐዎ ﻌ ዎﾐ)ﾉ', '(^≗ω≗^)',
   '(^・x・^)', '(=｀ェ´=)', '/ᐠ=ᆽ≠ ᐟ \\∫', '(=🝦 ﻌ 🝦=)', '(=🝦 ﻌ 🝦=)', '₍⸍⸌̣ʷ̣̫⸍̣⸌₎', '=＾ᵒ⋏ᵒ＾=', '(=〃ﻌ〃=)', '=＾ㅎ ⋏ ㅎ＾=', '=＾● ⋏ ●＾=', '=＾• ⋏ •＾=', '＾⌤＾', '̳ ៱˳_˳៱ ̳ ∫']
@@ -13,11 +13,11 @@ module.exports = class Cat {
     const part2 = start[Math.floor(Math.random() * start.length)]
     let part3 = end[Math.floor(Math.random() * end.length)]
     part3 = part3 === 'ordinal' ? ordinal() : part3
-    let full =part1 + part2 + part3
-    if(noSpace)[
+    let full = part1 + part2 + part3
+    if (noSpace) [
       full.replace(/ /g, '')
     ]
-    return full ? full : this.getName(noSpace)
+    return full.trim() ? full : this.getName(noSpace)
   }
 
   static getCat() {
@@ -25,10 +25,10 @@ module.exports = class Cat {
   }
 }
 function ordinal() {
-// eslint-disable-next-line prefer-const
+  // eslint-disable-next-line prefer-const
   let num = Math.ceil(Math.random() * 2)
   if (num === 1) {
-  // eslint-disable-next-line prefer-const
+    // eslint-disable-next-line prefer-const
     let n = Math.floor(Math.random() * 50)
     let res = ''
     if (n === 0) res = String(n)
